9e3394c9b1e50d198d2bd8c453113b7a211a6180
NICEDAY <abishekr182@gmail.com>
2023-07-26 22:16:07 +0530

    added eligiblity

---
+++ EligiblityTest.java
@@ -0,0 +1,18 @@
+public class EligiblityTest {
+    public static boolean isEligible(int a, int x, int y) {
+        return a >= x && a < y;
+    }
+    public static void main(String[] args) {
+
+        int a = 25;
+        int x= 18;
+        int y = 30;
+
+        if (isEligible(a, x, y)) {
+            System.out.println("Chef is eligible to take the exam!");
+        }
+        else {
+            System.out.println("Chef is not eligible to take the exam!");
+        }
+    }
+}
